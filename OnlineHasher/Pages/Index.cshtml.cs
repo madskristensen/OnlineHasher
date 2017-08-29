1dd2281fcc9db16b362cd92583a75efcb2fe3eb2
Mads Kristensen <post@madskristensen.net>
2017-08-29 13:38:59 -0700

    Add project files.

---
+++ OnlineHasher/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace OnlineHasher.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Hash { get; set; }
+
+        public void OnGet([FromQuery] string password, [FromQuery] string salt)
+        {
+            if (!string.IsNullOrEmpty(password))
+            {
+                byte[] saltBytes = string.IsNullOrWhiteSpace(salt) ? new byte[0] : Encoding.UTF8.GetBytes(salt);
+
+                byte[] hashBytes = KeyDerivation.Pbkdf2(
+                    password: password,
+                    salt: saltBytes,
+                    prf: KeyDerivationPrf.HMACSHA1,
+                    iterationCount: 1000,
+                    numBytesRequested: 256 / 8
+                );
+
+                Hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+            }
+        }
+    }
+}
